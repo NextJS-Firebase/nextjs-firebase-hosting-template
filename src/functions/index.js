63370af38b5fc70c042cd86fcb374441a77c44cc
scottie-schneider <scott@laclave.co>
2020-04-01 18:29:42 -0500

    hosting working, functions refactored

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,15 +1,7 @@
-const path = require('path')
-const functions = require('firebase-functions')
-const next = require('next')
+const admin = require("firebase-admin");
+const { auth, firestore, https, pubsub } = require("firebase-functions");
+admin.initializeApp();
 
-var dev = process.env.NODE_ENV !== 'production'
-var app = next({
-  dev,
-  conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` },
-})
-var handle = app.getRequestHandler()
-
-exports.next = functions.https.onRequest((req, res) => {
-  console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-  return app.prepare().then(() => handle(req, res))
-})
+// HTTPS Functions
+const h = require("./https");
+exports.next = https.onRequest(h.hosting.next);
